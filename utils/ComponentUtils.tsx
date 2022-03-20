5c5f3e9753e8e9eb22b50d78b1af7c1791cde392
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-20 19:46:19 +0200

    Improved component utils

--- utils/ComponentUtils.tsx
+++ utils/ComponentUtils.tsx
@@ -5,7 +5,7 @@ import {
     endsWith,
     filter,
     find,
-    has,
+    has, isString,
     keys,
     map,
     startsWith
@@ -18,6 +18,45 @@ export interface ComponentNodeMap {
 
 export class ComponentUtils {
 
+    public static prepareParagraphNodes (
+        t: TFunction,
+        description: any[] | undefined,
+        paramBlocks: ComponentNodeMap | undefined
+    ) {
+        return (
+            <>
+                {description
+                    ? map(description, (paragraph: string | ReactNode, i: number) => {
+
+                        if ( !isString(paragraph) ) {
+                            return (
+                                <p key={`paragraph-${i}`}>{paragraph}</p>
+                            );
+                        }
+
+                        if ( paramBlocks ) {
+                            return (
+                                <p key={`paragraph-${i}`}>{
+                                    ComponentUtils.prepareNodesFromString(
+                                        t(paragraph),
+                                        paramBlocks,
+                                        t
+                                    )
+                                }</p>
+                            );
+                        }
+
+                        return (
+                            <p key={`paragraph-${i}`}>{t(paragraph)}</p>
+                        );
+
+                    })
+                    : null}
+            </>
+        );
+
+    }
+
     /**
      * Replaces matching blocks from inputString.
      *
@@ -27,55 +66,55 @@ export class ComponentUtils {
      * @param translationParams
      */
     public static prepareNodesFromString (
-        inputString       : string,
-        blocks            : ComponentNodeMap,
-        t                 : TFunction,
-        translationParams : {readonly [key: string]: any} = {}
-    ) : ReactNode[] {
+        inputString: string,
+        blocks: ComponentNodeMap,
+        t: TFunction,
+        translationParams: {readonly [key: string]: any} = {}
+    ): ReactNode[] {
 
-        const startTag : string = '[[';
-        const endTag   : string = ']]';
+        const startTag: string = '[[';
+        const endTag: string = ']]';
 
-        let input : string = inputString;
+        let input: string = inputString;
 
-        if (input.length === 0) return [];
+        if ( input.length === 0 ) return [];
 
-        const blockKeywords : string[] = map(keys(blocks), (k) => `${startTag}${k}${endTag}`);
-        if (blockKeywords.length === 0) {
-            return [input];
+        const blockKeywords: string[] = map(keys(blocks), (k) => `${startTag}${k}${endTag}`);
+        if ( blockKeywords.length === 0 ) {
+            return [ input ];
         }
 
-        const itemsFound : ReactNode[] = [];
+        const itemsFound: ReactNode[] = [];
 
-        const createIndexPairFunc = (str : string) => {
-            return (k : string) : [string, number] => [k, str.indexOf(k)];
+        const createIndexPairFunc = (str: string) => {
+            return (k: string): [ string, number ] => [ k, str.indexOf(k) ];
         };
 
         do {
 
             const indexPairs = map(blockKeywords, createIndexPairFunc(input));
-            const indexes = map(indexPairs, (item: [string, number]) : number => item[1]);
+            const indexes = map(indexPairs, (item: [ string, number ]): number => item[1]);
             const index = ComponentUtils._findSmallestIndex(indexes);
-            if (index < 0) break;
+            if ( index < 0 ) break;
 
-            const foundPair : [string, number] | undefined = find(indexPairs, (item: [string, number]): boolean => item[1] === index);
-            const foundKeyword : string | undefined = foundPair ? foundPair[0] : undefined;
-            if (foundKeyword === undefined) throw new TypeError(`Could not find matching key for index ${index}`);
-            const foundBlockKey : string = startsWith(foundKeyword, startTag) && endsWith(foundKeyword, endTag) ? foundKeyword.substring(startTag.length, foundKeyword.length - endTag.length) : '';
-            if (!has(blocks, foundBlockKey)) throw new TypeError(`Could not find matching block for index ${index} and key ${foundBlockKey} from tag ${foundKeyword}`);
+            const foundPair: [ string, number ] | undefined = find(indexPairs, (item: [ string, number ]): boolean => item[1] === index);
+            const foundKeyword: string | undefined = foundPair ? foundPair[0] : undefined;
+            if ( foundKeyword === undefined ) throw new TypeError(`Could not find matching key for index ${index}`);
+            const foundBlockKey: string = startsWith(foundKeyword, startTag) && endsWith(foundKeyword, endTag) ? foundKeyword.substring(startTag.length, foundKeyword.length - endTag.length) : '';
+            if ( !has(blocks, foundBlockKey) ) throw new TypeError(`Could not find matching block for index ${index} and key ${foundBlockKey} from tag ${foundKeyword}`);
 
-            if (index !== 0) {
-                itemsFound.push( <>{t(input.substring(0, index), translationParams)}</> );
+            if ( index !== 0 ) {
+                itemsFound.push(<>{t(input.substring(0, index), translationParams)}</>);
                 input = input.substring(index);
             }
 
-            itemsFound.push( blocks[foundBlockKey] );
+            itemsFound.push(blocks[foundBlockKey]);
             input = input.substring(foundKeyword.length);
 
-        } while (true);
+        } while ( true );
 
-        if (input.length) {
-            itemsFound.push( <>{t(input, translationParams)}</> );
+        if ( input.length ) {
+            itemsFound.push(<>{t(input, translationParams)}</>);
         }
 
         return itemsFound;
@@ -84,9 +123,9 @@ export class ComponentUtils {
 
     private static _findSmallestIndex (
         indexes: number[]
-    ) : number {
-        indexes = filter(indexes, (i: number) : boolean => i >= 0);
-        indexes.sort((a : number, b: number) => a - b);
+    ): number {
+        indexes = filter(indexes, (i: number): boolean => i >= 0);
+        indexes.sort((a: number, b: number) => a - b);
         return indexes.shift() ?? -1;
     }
 
