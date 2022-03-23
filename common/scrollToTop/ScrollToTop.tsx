69c06d3f5e04d263a488f044c3fb28f6b3e8c79f
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-23 16:00:42 +0200

    Added check for window object existance

--- common/scrollToTop/ScrollToTop.tsx
+++ common/scrollToTop/ScrollToTop.tsx
@@ -2,32 +2,33 @@
 
 import { useLayoutEffect } from "react";
 import { BrowserOnly } from "../browserOnly/BrowserOnly";
-import { LogService } from "../../../../../hg/core/LogService";
+import { LogService } from "../../../../core/LogService";
 
-const LOG = LogService.createLogger('ScrollToTop');
+const LOG = LogService.createLogger('BrowserOnlyScrollToTop');
 
 /**
  * This method would print a warning on SSR React, so we use a useEffect workaround to remove that
  * warning. See the ScrollToTop() below.
  */
 function BrowserOnlyScrollToTop () {
-
     useLayoutEffect(() => {
-        LOG.debug(`BrowserOnlyScrollToTop: Scrolling to top`);
-        window.scrollTo(0, 0);
+        if (typeof window !== 'undefined') {
+            try {
+                LOG.debug(`Scrolling to top`);
+                window.scrollTo(0, 0);
+            } catch(err) {
+                LOG.error(`Exception: `, err);
+            }
+        } else {
+            LOG.warn(`Could not detect window object. Cannot scroll.`);
+        }
     }, []);
-
     return null;
-
 }
 
 /**
  * Scrolls to top when ever the route changes
  */
 export function ScrollToTop () {
-
     return <BrowserOnly><BrowserOnlyScrollToTop /></BrowserOnly>;
-
 }
-
-
