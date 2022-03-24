b05da8b424444ca49e4980f715d7ace1c4aea95d
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-24 19:43:38 +0200

    Refactored Form as ModelForm, and created new simpler Form component

--- constants/UserInterfaceClassName.ts
+++ constants/UserInterfaceClassName.ts
@@ -1,10 +1,11 @@
-
-
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 // Copyright (c) 2021. Sendanor <info@sendanor.fi>. All rights reserved.
 
 export enum UserInterfaceClassName {
 
     FORM               = "form",
+    MODEL_FORM         = "model-form",
+    SUBMIT_BUTTON      = "submit-button",
     TAB                = "tab",
     FIELD              = "field",
     CHECKBOX_FIELD     = "checkbox-field",
